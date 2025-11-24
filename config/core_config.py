d8ff4fe37c78bc29f26ba041d4e85512c0b668c5
root <ellis-langford>
2025-11-24 17:48:44 +0000

    Move regions to core config

--- config/core_config.py
+++ config/core_config.py
@@ -25,6 +25,11 @@ PARAMETERS = {
         "default": True,
         "help": "If True, the pipeline will run surface generation (default: True)"
     },
+    "run_mesh_generation": {
+        "type": bool,
+        "default": True,
+        "help": "If True, the pipeline will run mesh generation (default: True)"
+    },
     "run_mesh_mapping": {
         "type": bool,
         "default": True,
@@ -34,5 +39,12 @@ PARAMETERS = {
         "type": bool,
         "default": True,
         "help": "If True, the pipeline will run MPET modellling (default: True)"
+    },
+    "regions": {
+        "type": str,
+        "default": "cerebrum_L,cerebrum_R,cerebrumWM_L,cerebrumWM_R," +
+                   "cerebellum_L,cerebellum_R,cerebellumWM_L,cerebellumWM_R," +
+                   "brainstem_L,brainstem_R,ventricles,wholebrain",
+        "help": "Comma seperated list of regions to process"
     },
 }
