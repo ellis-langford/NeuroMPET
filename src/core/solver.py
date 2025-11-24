0311e2ec006f07078571678f39db2440916d621f
ellis-langford <ellis.langford@gmail.com>
2025-11-24 17:58:57 +0000

    Move logging to core solver code. Update outputs folder

--- src/core/solver.py
+++ src/core/solver.py
@@ -9,9 +9,10 @@ class Solver(object):
     """Class setup"""
     def __init__(self, plugin_obj):
         # Check all expected attributed are present
-        to_inherit = ["loggers", "parameters", 
-                      "base_dir", "input_dir", 
-                      "output_dir", "log_dir", "boundary_conditions"]
+        to_inherit = ["loggers", "parameters", "base_dir", 
+                      "input_dir", "interim_dir", "output_dir", 
+                      "log_dir", "surface_dir", "mesh_dir",
+                      "labels_file", "bc_file"]
         for attr in to_inherit:
             try:
                 setattr(self, attr, getattr(plugin_obj, attr))
@@ -30,11 +31,15 @@ class Solver(object):
         self.timestep_count = self.parameters["timestep_count"]
         self.timestep_interval = self.parameters["timestep_interval"]
 
+        # Outdir
+        self.modelling_outdir = os.path.join(self.output_dir, "modelling")
+        os.makedirs(self.modelling_outdir, exist_ok=True)
+
         # Define command
         self.solver_command = f"make clean && make && " + \
-                              f"./MPET3D '{self.bit_file}' '{self.output_dir}' " + \
+                              f"./MPET3D '{self.bit_file}' '{self.modelling_outdir}' " + \
                               f"{self.timestep_size} {self.timestep_count} " + \
-                              f"{self.timestep_interval} '{self.boundary_conditions}' " + \
+                              f"{self.timestep_interval} '{self.bc_file}' " + \
                               f"'{self.labels_file}'"
 
         # Define location of MPET source code
@@ -52,14 +57,7 @@ class Solver(object):
                                             self.solver_command],
                                             cwd=self.source_code_dir,
                                             stdout=outfile,
-                                            stderr=subprocess.STDOUT,
-                                            text=True,
-                                            bufsize=1)
-            for line in solver_sub.stdout:
-                timestamp = datetime.datetime.now().strftime("%d-%m-%& %H:%M")
-                outfile.write(f"[ Log | {timestamp} ] {line}")
-                outfile.flush()
-            solver_sub.wait()
+                                            stderr=subprocess.STDOUT)
             
         if solver_sub.returncode != 0:
             self.loggers.errors(f"Solver execution returned non-zero exit status - " +
@@ -67,12 +65,12 @@ class Solver(object):
 
         # Check required outputs have been produced
         for timestep in range(0, int(self.timestep_count * 50 / self.timestep_interval)):
-            result_file = os.path.join(self.output_dir, f"ouputs_{timestep}.vtu")
+            result_file = os.path.join(self.modelling_outdir, f"ouputs_{timestep}.vtu")
 
             if not os.path.exists(result_file):
                 self.loggers.errors(f"Solver has not produced an output file at timestep {timestep} " +
                                     f"- please check log file at {self.solver_log}")
-            elif not os.path.exists(os.path.join(self.output_dir, f"ouputs_region.vtu")):
+            elif not os.path.exists(os.path.join(self.modelling_outdir, f"ouputs_region.vtu")):
                 self.loggers.errors(f"Solver has not produced a regional output file " +
                                     f"- please check log file at {self.solver_log}")
 
@@ -81,33 +79,29 @@ class Solver(object):
         Running modelling processing
         """
         self.loggers.plugin_log("Running MPET solver")
+        self.interim_dir = os.path.join(self.interim_dir, "modelling")
+        os.makedirs(self.interim_dir, exist_ok=True)
         
-        # Set labels file path depending if meshing run
-        if self.parameters["run_mesh_mapping"]:
-            self.labels_file = os.path.join(self.output_dir, "labels.txt")
-        else:
-            self.labels_file = os.path.join(self.input_dir, "labels.txt")
+        # Define meshes
+        self.mesh_file = glob.glob(os.path.join(self.mesh_dir, "**", "global.vtk"), recursive=True)[0]
         
-        # Run MPET solver
-        if self.parameters["run_modelling"]:
-            # Define meshes
-            self.mesh_dir = os.path.join(self.input_dir, "meshes")
-            self.mesh_file = os.path.join(self.mesh_dir, "global", "global.vtk")
-            
-            # Define surface files
-            self.surface_dir = os.path.join(self.input_dir, "surfaces")
-            self.wb_surface = glob.glob(os.path.join(self.surface_dir, "**", "*wholebrain*.stl"), recursive=True)[0]
-            self.vent_surface = glob.glob(os.path.join(self.surface_dir, "**", "*ventricles*.stl"), recursive=True)[0]
+        # Define surface files
+        self.wb_surface = glob.glob(os.path.join(self.surface_dir, "**", "*wholebrain*.stl"), recursive=True)[0]
+        self.vent_surface = glob.glob(os.path.join(self.surface_dir, "**", "*ventricles*.stl"), recursive=True)[0]
 
-            # Produce custom .bit file
-            self.loggers.plugin_log("Creating custom .bit file")
-            self.bit_file = os.path.join(self.input_dir, "global.bit")
-            self.mesh_loader.convert_and_export_custom_mesh(self.mesh_file,
-                                                            self.wb_surface,
-                                                            self.vent_surface,
-                                                            self.bit_file)
+        # Produce custom .bit file
+        self.loggers.plugin_log("Creating custom .bit file")
+        self.bit_file = os.path.join(self.input_dir, "global.bit")
+        self.mesh_loader.convert_and_export_custom_mesh(self.mesh_file,
+                                                        self.wb_surface,
+                                                        self.vent_surface,
+                                                        self.bit_file)
+        
+        # Produce inner and outer vtu files for visualisation
+        self.mesh_loader.extract_surfaces_from_bit(self.bit_file,
+                                                   self.interim_dir)
 
-            # Run solver
-            self.loggers.plugin_log("Running MPET solver")
-            self.build_solver_command() # Build solver command
-            self.run_solver() # Run Solver
+        # Run solver
+        self.loggers.plugin_log("Running MPET solver")
+        self.build_solver_command() # Build solver command
+        self.run_solver() # Run Solver
